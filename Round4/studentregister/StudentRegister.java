b6f02ba69acce336b2c60e09f6cfa2f0db780ad5
ftonvi <onni.vitikainen@tuni.fi>
2022-01-28 13:40:06 +0200

    Student Register

--- Round4/studentregister/StudentRegister.java
+++ Round4/studentregister/StudentRegister.java
@@ -1,118 +1,79 @@
-import java.io.*;
-import java.util.*;
-import java.lang.String;
-
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class StudentRegister {
-    private final ArrayList<Student> students = new ArrayList<>();
-    private final ArrayList<Course> courses = new ArrayList<>();
-    private final ArrayList<Attainment> attainments = new ArrayList<>();
+    ArrayList<Student> students = new ArrayList<>();
+    ArrayList<Course> courses = new ArrayList<>();
+    ArrayList<Attainment> attainments = new ArrayList<>();
 
-
-
-    StudentRegister(){
+    StudentRegister() {
 
     }
 
-    public ArrayList<Student> getStudents(){
-        students.sort((a,b) -> a.getName().compareTo(b.getName()));
-        // students.sort((Student a, Student b)-> a.getName().compareTo(b.getName());
-        //Collections.sort(students, (Student a, Student b)-> a.getName().compareTo(b.getName()));
-        return students;
-
-
+    public ArrayList<Student> getStudents() {
+        students.sort(Comparator.comparing(Student::getName));
+        return this.students;
     }
 
-    public ArrayList<Course> getCourses(){
-        courses.sort((a,b) -> a.getName().compareTo(b.getName()));
-        // students.sort((Student a, Student b)-> a.getName().compareTo(b.getName());
-        //Collections.sort(courses, (Course a, Course b)-> a.getName().compareTo(b.getName()));
-        return courses;
-
-
+    public ArrayList<Course> getCourses() {
+        courses.sort(Comparator.comparing(Course::getName));
+        return this.courses;
     }
 
-
-    public void addStudent(Student student){
-        if(!students.contains(student)){
+    public void addStudent(Student student) {
+        if(!students.contains(student)) {
             students.add(student);
         }
     }
 
-    public void addCourse(Course course){
-        if(!courses.contains(course)){
+    public void addCourse(Course course) {
+        if(!courses.contains(course)) {
             courses.add(course);
         }
-
     }
 
-    public void addAttainment(Attainment att){
-        if(!attainments.contains(att)){
+    public void addAttainment(Attainment att) {
+        if(!attainments.contains(att)) {
             attainments.add(att);
         }
     }
 
-
-
-    public void printStudentAttainments(String studentNumber, String order){
-        if(order == "by name"){
-            courses.sort((a,b) -> a.getName().compareTo(b.getName()));
-            printStudentAttainments(studentNumber);
+    public void printStudentAttainments(String studentNumber, String order) {
+        if(order.equals("by name")) {
+            courses.sort(Comparator.comparing(Course::getName));
         }
-        if(order == "by code"){
-            courses.sort((a,b) -> a.getCode().compareTo(b.getCode()));
-            printStudentAttainments(studentNumber);
+        if(order.equals("by name")) {
+            courses.sort(Comparator.comparing(Course::getCode));
         }
-        
+        printStudentAttainments(studentNumber);
     }
 
-    public void printStudentAttainments(String studentNumber){
-        List<String> name_check = new ArrayList<>();
-        for(Student s : students){
-            if (s.getStudentNumber() == studentNumber){
-                name_check.add(studentNumber);
+    public void printStudentAttainments(String studentNumber) {
+        List<String> isStudent = new ArrayList<>();
+
+        for(Attainment att : attainments) {
+            if (att.getStudentNumber().equals(studentNumber)) {
+                isStudent.add(studentNumber);
             }
         }
-        if (name_check.size() < 1){
-            System.out.format("Unknown student number: %s%n",studentNumber);
-        }
-        for (Student s : students){
-            if (s.getStudentNumber() == studentNumber){
-                System.out.format("%s (%s):%n", s.getName(), s.getStudentNumber());
-                for (Attainment att : attainments){
-                    if(att.getStudentNumber() == studentNumber){
-                        for (Course c: courses){
-                            if (att.getCourseCode() == c.getCode()){
-                                System.out.format("%s %s: %d%n", c.getCode(), c.getName(), att.getGrade());
+        if(isStudent.size() < 1) {
+            System.out.printf("Unknown student number: %s\n", studentNumber);
+        } else {
+            for(Student s : students) {
+                if(s.getStudentNumber().equals(studentNumber)) {
+                    System.out.printf("%s (%s):\n", s.getName(), studentNumber);
+                    for(Course c : courses) {
+                        for(Attainment att : attainments) {
+                            if(att.getStudentNumber().equals(studentNumber)) {
+                                if(att.getCourseCode().equals(c.getCode())) {
+                                    System.out.printf("  %s %s: %d\n", c.getCode(), c.getName(), att.getGrade());
+                                }
                             }
                         }
                     }
                 }
             }
         }
-
-        for (Attainment att : attainments){
-            if(att.getStudentNumber() == studentNumber){
-                System.out.println("Testi");
-            }
-        }
-
-
-
-
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
