5a128893e1cbdcb3cd61c0dbd63d093433f85de9
ftonvi <onni.vitikainen@tuni.fi>
2022-02-03 14:46:58 +0200

    datetime

--- Round5/datetime/Date.java
+++ Round5/datetime/Date.java
@@ -16,6 +16,41 @@ public class Date {
 
         String datestring = String.format("%02d.%02d.%d", day, month ,year);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.uuuu");
+
+        if (year % 4 == 0){
+            if (year % 100 == 0){
+                if (year % 400 == 0){
+                    if (month == 2){
+                        if(day > 29){
+                            throw new DateException(String.format("Illegal date %02d.%02d.%d", day, month ,year));
+                        }
+                    }
+                }
+            }
+        }
+
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month ==8 || month ==10 || month == 12){
+            if (day > 31){
+                throw new DateException(String.format("Illegal date %02d.%02d.%d", day, month ,year));
+            }
+        }
+
+        else if (month == 4 || month == 6 || month == 9 || month == 11){
+            if (day > 30){
+                throw new DateException(String.format("Illegal date %02d.%02d.%d", day, month ,year));
+            }
+        }
+
+        else{
+            if(year % 4 == 0){
+                if (day > 29){
+                    throw new DateException(String.format("Illegal date %02d.%02d.%d", day, month ,year));
+                }
+                else if (day > 28) {
+                    throw new DateException(String.format("Illegal date %02d.%02d.%d", day, month ,year));
+                }
+            }
+        }
         try {
             formatter.parse(datestring);
         }
