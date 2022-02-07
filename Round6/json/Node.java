1e6c5cda302c469de428f0bf514609118700012c
Terhi Kilamo <terhi.kilamo@tuni.fi>
2022-02-07 13:50:58 +0200

    JSON test files

---
+++ Round6/json/Node.java
@@ -0,0 +1,69 @@
+public abstract class Node {
+  public boolean isValue() {
+    return this instanceof ValueNode;
+  }
+
+  public boolean isArray() {
+    return this instanceof ArrayNode;
+  }
+
+  public boolean isObject() {
+    return this instanceof ObjectNode;
+  }
+  
+  public void printSimple() {
+    StringBuilder sb = new StringBuilder();
+    printSimple(this, sb);
+    System.out.print(sb.toString());
+  }
+
+  public void printJson() {
+    throw new UnsupportedOperationException("printJson has not been implemented!");
+  }
+
+  private static final String NL = System.lineSeparator();
+
+  private static String numberToString(Double d) {
+    String str = Double.toString(d);
+    if(str.endsWith(".0")) {
+      str = str.substring(0, str.length() - 2);
+    }
+    return str;
+  }
+
+  private void printSimple(Node node, StringBuilder sb) {
+    if(node.isObject()) {
+      sb.append("ObjectNode").append(NL);
+      ObjectNode objNode = (ObjectNode) node;
+      for(String name : objNode) {
+        sb.append(name).append(": ");
+        printSimple(objNode.get(name), sb);
+      }
+    }
+    else if(node.isArray()) {
+      sb.append("ArrayNode").append(NL);
+      ArrayNode arrNode = (ArrayNode) node;
+      for(Node aNode : arrNode) {
+        printSimple(aNode, sb);
+      }
+    }
+    else if(node.isValue()) {
+      ValueNode valNode = (ValueNode) node;
+      String typeStr = "NullValue";
+      String valStr = "null";
+      if(valNode.isNumber()) {
+        typeStr = "NumberValue";
+        valStr = numberToString(valNode.getNumber());
+      }
+      else if(valNode.isBoolean()) {
+        typeStr = "BooleanValue";
+        valStr = Boolean.toString(valNode.getBoolean());
+      }
+      else if(valNode.isString()) {
+        typeStr = "StringValue";
+        valStr = "\"" + valNode.getString() + "\"";
+      }
+      sb.append(String.format("%s(%s)%n", typeStr, valStr));
+    }
+  }
+}
