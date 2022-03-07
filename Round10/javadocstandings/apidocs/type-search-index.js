3c8336fbcde93f623bead7a62a98d920a90fcbda
heikki <heikki.hyyro@tuni.fi>
2022-03-07 19:22:30 +0200

    Material for Standings & JavaDoc.

---
+++ Round10/javadocstandings/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"fi.tuni.prog3.standings","l":"Standings"},{"p":"fi.tuni.prog3.standings","l":"Standings.Team"}];updateSearchResults();
