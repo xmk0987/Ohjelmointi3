f04722f7589b04304f742177e4f1174ca50db743
ftonvi <onni.vitikainen@tuni.fi>
2022-03-09 15:03:49 +0200

    Round8

---
+++ Round8/sevenzipsearch/src/main/java/sevenznimi.java
@@ -0,0 +1,61 @@
+import org.apache.commons.compress.archivers.sevenz.SevenZArchiveEntry;
+import org.apache.commons.compress.archivers.sevenz.SevenZFile;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.lang.String;
+
+
+public class sevenznimi {
+    public static void main(String[] args)throws IOException{
+
+        String strFilename = args[0];
+        String strHakusana = args[1];
+
+        try(SevenZFile archiveFile = new SevenZFile(new File(strFilename))){
+            SevenZArchiveEntry entry;
+            while ((entry = archiveFile.getNextEntry() ) != null) {
+
+                if(entry.getName().contains("txt")){
+
+                    ByteArrayOutputStream contentBytes = new ByteArrayOutputStream();
+
+
+                    byte[] buffer = new byte[2048];
+                    int bytesRead;
+                    while((bytesRead = archiveFile.read(buffer)) != -1) {
+                        contentBytes.write(buffer, 0, bytesRead);
+                    }
+
+                    String content = contentBytes.toString("UTF-8");
+                    System.out.println(entry.getName());
+                    try(var reader = new BufferedReader((new StringReader(content)))){
+                        String line;
+                        int i = 1;
+                        while((line = reader.readLine()) != null){
+                            String line2 = line.toLowerCase();
+                            if(line2.contains(strHakusana.toLowerCase())){
+                                System.out.println(line.replaceAll("(?i)" + strHakusana, strHakusana.toUpperCase()));
+
+                            }
+                            i+= 1;
+
+                        }
+                    }
+
+                }
+                System.out.println();
+
+            }
+
+        }
+        }
+
+
+
+
+
+
+}
+
