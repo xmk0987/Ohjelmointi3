5d874b79c550f8638a90bdebce08ae8a5fc69dca
ftonvi <onni.vitikainen@tuni.fi>
2022-03-14 13:08:12 +0200

    GIT_EX double

--- Round10/GIT_EX/temperature.py
+++ Round10/GIT_EX/temperature.py
@@ -1,6 +1,8 @@
 
 def main():
 
+    command = input("Select transform direction (CF) or (FC): ")
+
     try:
         input_temp = float(input("Input temperature: "))
     except:
@@ -8,7 +10,11 @@ def main():
         exit()
 
 
-    print("{} C = {} F".format(input_temp, t_transform(input_temp, 'c')))
+    if command == "CF":
+        print("{} C = {} F".format(input_temp, "{:.1f}".format(t_transform(input_temp, 'c'))))
+    if command == "FC":
+        print("{} F = {} C".format(input_temp, "{:.1f}".format(t_transform(input_temp, 'f'))))
+
 
 
 
@@ -18,7 +24,8 @@ def t_transform(temp, type):
 
     if type == 'c':
         return float(temp * 9/5 + 32)
-
+    if type == 'f':
+        return float((temp - 32)/(9/5))
 
     return None
 
