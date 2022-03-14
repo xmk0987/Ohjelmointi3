2fecd2dc07d6e0c607931517efa367b6a46ed3a9
ftonvi <onni.vitikainen@tuni.fi>
2022-03-14 12:55:47 +0200

    git_ex

--- Round10/GIT_EX/temperature.py
+++ Round10/GIT_EX/temperature.py
@@ -9,7 +9,7 @@ def main():
 
 
     print("{} C = {} F".format(input_temp, t_transform(input_temp, 'c')))
-    print("{} F = {} C".format(input_temp, t_transform(input_temp, 'f')))
+
 
 
 
@@ -18,8 +18,7 @@ def t_transform(temp, type):
 
     if type == 'c':
         return float(temp * 9/5 + 32)
-    if type == 'f':
-        return float((temp - 32)/(9/5))
+
 
     return None
 
